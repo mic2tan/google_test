700260a81502d90b1b83056219800807abe8479c
mic2tan <mic2tan@gmail.com>
2014-04-24 18:49:52 -0500

    google test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleTest::Application.config.secret_key_base = 'b75a50dfd2ed2ffa82ec75b55cb6b9b8a2e3d4adae99e88c01a37a29bf7633ca8d01753f881c064e3cc3aa4cf861d2a48a074acb75ad45053bbba18d90d8cb8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoogleTest::Application.config.session_store :cookie_store, key: '_google_test_session'
